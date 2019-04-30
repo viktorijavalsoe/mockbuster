fc9b8a85417cbe3637a7fe7169ac88961250424b
Viktorija Valsoe <johndoe@example.com>
2019-04-30 17:26:31 +0200

    Installed npm @ng-bootstrap, and imported NgbModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CheckoutComponent } from './checkout/checkout.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
@@ -25,14 +26,16 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     CheckoutComponent,
     OrderSuccessComponent,
     AdminOrdersComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
